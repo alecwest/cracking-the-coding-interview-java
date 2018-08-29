f5bd47af100ada852b6c67c46a449b28c4cbb7db
Alec West <alecwest38@gmail.com>
2018-08-29 08:05:43 -0500

    use better mask

--- src/test/java/bitmanipulation/PairwiseSwap.java
+++ src/test/java/bitmanipulation/PairwiseSwap.java
@@ -2,11 +2,6 @@ package bitmanipulation;
 
 public class PairwiseSwap {
     public static int pairWiseSwap(Integer value) {
-        Integer mask = 0;
-        // odd bits first
-        for (int i = 0; i < Integer.BYTES * 4; i += 2) {
-            mask += (int) Math.pow((double)2, (double)i);
-        }
-        return ((value & mask) << 1) | ((value & (mask << 1)) >> 1);
+        return ((value & 0x55555555) << 1) | ((value & 0xAAAAAAAA) >>> 1);
     }
 }
